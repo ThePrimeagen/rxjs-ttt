aae5169d2719466707aa4aecf8571dd8eb0f8748
Primeagen <michael.b.paulson@gmail.com>
2013-12-25 20:54:54 -0800

    PWNED:

--- js/main.js
+++ js/main.js
@@ -9,46 +9,41 @@ var TicTacToe = function($element) {
     var g = graph($element, {});
     var player = 0;
     var currentNode = g[0][0].setPlayer(player).enter();
+    var gameWinObservable = winnerObservable(g);
+    var controlsObservable = setupControls(gameWinObservable, currentNode);
 
-    // Listens to the direction
-    Direction.onKeyboardDirection.subscribe(function(direction) {
-        currentNode = currentNode
-            .exit()
-            .getNeighbor(direction)
-            .setPlayer(player)
-            .enter();
+    controlsObservable.subscribe(function(val) {
     });
 
-    var enter = rxKeyboard(13);
-
-    enter
-        .filter(function() {
-            return !currentNode.isSet();
-        })
-        .subscribe(function(code) {
-            currentNode.check();
-            player = (++player) % 2;
-
-            // Checks for winner.
-            var winner = checkGraphForWinner(g);
-            if (winner) {
-                // what to do?
-                console.log('Found winner.');
-                for (var i = 0; i < winner.nodes.length; i++) {
-                    console.log(winner.nodes[i]._name);
-                }
-            }
-        });
-
-    enter
-        .filter(function() {
-            return currentNode.isSet();
-        })
-        .subscribe(function(code) {
-            // what to do ?
-            console.log('Oh no!');
+    gameWinObservable
+        .subscribe(function(winData) {
+            var nodes = winData.nodes;
+            console.log(nodes);
         });
 };
+
+function setupControls(gameWinningObservable, firstNode) {
+    var player = 0;
+
+    return Rx.Observable.merge(Direction.onKeyboardDirection, rxKeyboard(13))
+        .takeUntil(gameWinningObservable)
+        .scan(firstNode, function(currentNode, val) {
+
+            // enter key pressed
+            if (val === 13) {
+                currentNode.check();
+                player = (++player) % 2;
+                return currentNode;
+            } else {
+                // Direction key pressed
+                return currentNode
+                        .exit()
+                        .getNeighbor(val)
+                        .setPlayer(player)
+                        .enter();
+            }
+        });
+}
 
 module.exports = TicTacToe;
 
@@ -70,34 +65,52 @@ function checkForWin(nodes) {
     return true;
 }
 
-function checkGraphForWinner(graph) {
-    // check rows and columns
-    var topL = [], topR = [];
-    var checks = [topL, topR];
-    for (var r = 0; r < graph.length; r++) {
-        var row = graph[r];
-        topL.push(row[0 + r]);
-        topR.push(row[row.length - (1 + r)]);
+/**
+ * Checks for a winner of the graph.
+ * @param graph
+ */
+function winnerObservable(graph) {
 
-        checks.push(row);
-    }
+    return Rx.Observable.create(function(observer) {
+        var enter = rxKeyboard(13);
+        enter.subscribe(function() {
+            var res = isThereWinner();
+            if (res) {
+                observer.onNext(res);
+                observer.onCompleted();
+            }
+        });
 
-    for (var c = 0; c < graph.length; c++) {
-        var col = [];
-        for (var r = 0; r < graph.length; r++) {
-            col.push(graph[r][c]);
+        function isThereWinner() {
+            // check rows and columns
+            var topL = [], topR = [];
+            var checks = [topL, topR];
+            for (var r = 0; r < graph.length; r++) {
+                var row = graph[r];
+                topL.push(row[0 + r]);
+                topR.push(row[row.length - (1 + r)]);
+
+                checks.push(row);
+            }
+
+            for (var c = 0; c < graph.length; c++) {
+                var col = [];
+                for (var r = 0; r < graph.length; r++) {
+                    col.push(graph[r][c]);
+                }
+
+                checks.push(col);
+            }
+
+            for (var i = 0; i < checks.length; i++) {
+                if (checkForWin(checks[i])) {
+                    return {
+                        nodes: checks[i]
+                    };
+                }
+            }
+
+            return false;
         }
-
-        checks.push(col);
-    }
-
-    for (var i = 0; i < checks.length; i++) {
-        if (checkForWin(checks[i])) {
-            return {
-                nodes: checks[i]
-            };
-        }
-    }
-
-    return false;
+    });
 }
